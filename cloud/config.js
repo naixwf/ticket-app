52844bcdbc754b57d51b8c0de5fe78f4f53d5390
naixwf <naixwf@gmail.com>
2014-08-29 09:35:41 +0800

    init

--- cloud/config.js
+++ cloud/config.js
@@ -2,11 +2,11 @@
  * Created by lzw on 14-8-19.
  */
 //我申请的一个应用的appId,appKey，你可以替换成自己的，新建一条工单，便可以看到相应的表
-exports.applicationId='my7clww2hnrairocm6uwus69fg806z50vvmtaz7oughnfq94';
-exports.applicationKey='npn86d4vj3ttgl0qemc972v70wrs1oimye04w2x3sb4r0zl7';
+exports.applicationId='nl6xyr7qi69vuaigujf3u9owty3jgzi6hn4fnl6hae96cgz7';
+exports.applicationKey='x1sroe57fomnw97mgew34j9w0xqg1iihgv4apq4w51ee0fcn';
 
 //express用做加密token的salt，自己申请应用搭建时，可稍微更改字符串，可以更安全
-exports.cookieParserSalt="klp4e8b4sddjp2";
+exports.cookieParserSalt="klp4e8b4sddjp4";
 
 //mailGun，一个邮箱服务提供商，当有新工单创建或有回复的时候用到
 exports.mailGunKey='';//please use your mailGunKey
@@ -15,7 +15,7 @@ exports.mailGunKey='';//please use your mailGunKey
 exports.slackUrl='https://avoscloud.slack.com/services/hooks/incoming-webhook?token=rNDqBLRC8TlG4YkPKBZSe2qB';
 
 //配置自己申请的子域名
-exports.hostUrl='http://myticket.avosapps.com';
+exports.hostUrl='http://hry.avosapps.com';
 
 //邮件的发送者，平时管理员回复了你的工单的时候，你便会收到来自这个邮箱的email
 exports.emailHost='notification@avoscloud.com';
